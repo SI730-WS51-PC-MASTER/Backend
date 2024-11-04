be47c02e4e4c4efbb16ba759d50b6ed12cc5e748
diego5m <sorianodiego2020@gmail.com>
2024-11-04 15:38:03 -0500

    feat: orders Application

--- Backend/Orders/Application/Internal/CommandServices/CartCommandService.cs
+++ Backend/Orders/Application/Internal/CommandServices/CartCommandService.cs
@@ -1,6 +1,34 @@
+using Backend.Orders.Domain.Model.Aggregates;
+using Backend.Orders.Domain.Model.Commands;
+using Backend.Orders.Domain.Repositories;
+using Backend.Orders.Domain.Services;
+using Backend.Shared.Domain.Repositories;
+
 namespace Backend.Orders.Application.Internal.CommandServices;
 
-public class CartCommandService
+public class CartCommandService(ICartRepository cartRepository,
+    IUnitOfWork unitOfWork) : ICartCommandService
 {
-    
+    public async Task<Cart?> Handle(CreateCartCommand command)
+    {
+        var cart =
+            await cartRepository.FindByComponentIdAsync(command.ComponentId);
+        if (cart != null)
+            throw new Exception("Cart with componentId already exists");
+
+        cart = new Cart(command);
+
+        try
+        {
+            await cartRepository.AddAsync(cart);
+            await unitOfWork.CompleteAsync();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+        return cart;
+    }
 }
--- Backend/Orders/Application/Internal/QueryServices/CartQueryService.cs
+++ Backend/Orders/Application/Internal/QueryServices/CartQueryService.cs
@@ -1,6 +1,14 @@
+using Backend.Orders.Domain.Model.Aggregates;
+using Backend.Orders.Domain.Model.Queries;
+using Backend.Orders.Domain.Repositories;
+using Backend.Orders.Domain.Services;
+
 namespace Backend.Orders.Application.Internal.QueryServices;
 
-public class CartQueryService
+public class CartQueryService(ICartRepository cartRepository) : ICartQueryService
 {
-    
+    public async Task<IEnumerable<Cart>> Handle(GetCartsByUserId query)
+    {
+        return await cartRepository.FindByUserIdAsync(query.UserId);
+    }
 }
