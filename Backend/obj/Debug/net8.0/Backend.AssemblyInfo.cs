1ce71b6f5d24511204d7c6fc713d5551ba78f4f9
Aranda Vallejos, Oscar Gabriel <134239824+OscarGAV@users.noreply.github.com>
2024-11-04 14:45:00 -0500

    feat: Update Dev

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a5a9edb057eb6428d531d680e7fb4309545415e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6116d3c84660af3f56f1a1452dd37b431b7494e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
